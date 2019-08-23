238505507e0d2dd00cfa2a78c9b7f42f184e9dfd
erikporksen <erikapoerksen@hotmail.com>
2019-08-23 13:18:19 +0200

    testing

--- src/main/java/com/CAM/HelperTools/FileOperations.java
+++ src/main/java/com/CAM/HelperTools/FileOperations.java
@@ -111,6 +111,7 @@ public class FileOperations {
     public static void renameDirectory(String oldPath, String newName){
         //File dir = new File(oldPath);
         //File newDir = new File(dir.getParent() + "\\" + newName);
+        deleteDirectory(oldPath);
         Path source = Paths.get(oldPath);
         try {
             Files.move(source, source.resolveSibling(newName));
