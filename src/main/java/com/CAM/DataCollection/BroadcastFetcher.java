ade7a2011988cd556e2531d75511eaf0490e14f1
Erik Andreas Pørksen <erik.andreas.poerksen@gmail.com>
2019-09-24 21:04:38 +0200

    fixed HtmlPage to TextPage

--- src/main/java/com/CAM/DataCollection/BroadcastFetcher.java
+++ src/main/java/com/CAM/DataCollection/BroadcastFetcher.java
@@ -1,10 +1,7 @@
 package com.CAM.DataCollection;
 
 import com.CAM.HelperTools.Log;
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.CookieManager;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.IOException;
@@ -13,13 +10,13 @@ public class BroadcastFetcher {
 
     private final String broadcastUrl = "https://raw.githubusercontent.com/erikabp123/ClassicAddonManager/master/EmergencyBroadcast.txt";
 
-    public HtmlPage scrape(String targetUrl) throws IOException {
+    public TextPage scrape(String targetUrl) throws IOException {
         WebClient client = new WebClient(BrowserVersion.CHROME);
         client.getOptions().setJavaScriptEnabled(false);
         client.getOptions().setCssEnabled(false);
         client.getOptions().setUseInsecureSSL(true);
 
-        HtmlPage page;
+        TextPage page;
 
         page = client.getPage(targetUrl);
 
@@ -27,7 +24,7 @@ public class BroadcastFetcher {
     }
 
     public String fetchBroadcastMessage() throws IOException{
-        HtmlPage page = scrape(broadcastUrl);
-        return page.asText();
+        TextPage page = scrape(broadcastUrl);
+        return page.getContent();
     }
 }
