61cfe8b6c9f72c6763ee6938c62c54d5a8f5b901
Erik Andreas Pørksen <erik.andreas.poerksen@gmail.com>
2019-09-30 20:34:16 +0200

    changed version number

--- src/main/java/com/CAM/Updating/VersionInfo.java
+++ src/main/java/com/CAM/Updating/VersionInfo.java
@@ -5,7 +5,7 @@ import com.google.gson.Gson;
 import java.io.*;
 
 public class VersionInfo {
-    public static final double CAM_VERSION = 0.75;
+    public static final double CAM_VERSION = 0.76;
     public static final double EXE_VERSION = 0.1;
     public static final double AUTOUPDATER_VERSION = 0.2;
 
